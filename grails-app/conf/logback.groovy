e2f950d2af38d4e62cb29dff9fe786b46e41d156
Marcel Samyn <marcel.samyn@lab900.com>
2022-06-20 08:51:38 +0200

    🔧 config(logback): always log to console for containers

--- grails-app/conf/logback.groovy
+++ grails-app/conf/logback.groovy
@@ -13,65 +13,17 @@ final CAS = 'CAS'
 final DEBUG_LOG = 'DEBUG_LOG'
 final APPENDERS = [[name: TOMCAT_LOG, suffix: ''],[name:ACCESS, suffix: '-session-access'],[name: CAS, suffix: '-cas'],[name: DEBUG_LOG, suffix: '-debug']]
 for (def a : APPENDERS) {
-    switch (Environment.current) {
-        case Environment.PRODUCTION:
-            appender(a.name, RollingFileAppender) {
-                file = "$loggingDir/$appName${a.suffix}.log"
-                encoder(PatternLayoutEncoder) {
-                    pattern =
-                            '%d{yyyy-MM-dd HH:mm:ss.SSS} ' + // Date
-                                    '%5p ' + // Log level
-                                    '--- [%15.15t] ' + // Thread
-                                    '%-40.40logger{39} : ' + // Logger
-                                    '%m%n%wex' // Message
-                }
-                rollingPolicy(FixedWindowRollingPolicy) {
-                    fileNamePattern = "$loggingDir/$appName${a.suffix}.%i.log.gz"
-                    minIndex=1
-                    maxIndex=4
-                }
-                triggeringPolicy(SizeBasedTriggeringPolicy) {
-                    maxFileSize = FileSize.valueOf('10MB')
-                }
-            }
-            break
-        case Environment.TEST:
-            appender(a.name, RollingFileAppender) {
-                file = "$loggingDir/$appName${a.suffix}.log"
-                encoder(PatternLayoutEncoder) {
-                    pattern =
-                            '%d{yyyy-MM-dd HH:mm:ss.SSS} ' + // Date
-                                    '%5p ' + // Log level
-                                    '--- [%15.15t] ' + // Thread
-                                    '%-40.40logger{39} : ' + // Logger
-                                    '%m%n%wex' // Message
-                }
-                rollingPolicy(FixedWindowRollingPolicy) {
-                    fileNamePattern = "$loggingDir/$appName${a.suffix}.%i.log.gz"
-                    minIndex=1
-                    maxIndex=4
-                }
-                triggeringPolicy(SizeBasedTriggeringPolicy) {
-                    maxFileSize = FileSize.valueOf('1MB')
-                }
-            }
-            break
-        case Environment.DEVELOPMENT:
-        default:
-            appender(a.name, ConsoleAppender) {
-                encoder(PatternLayoutEncoder) {
-                    pattern = '%clr(%d{yyyy-MM-dd HH:mm:ss.SSS}){faint} ' + // Date
-                            '%clr(%5p) ' + // Log level
-                            '%clr(---){faint} %clr([%15.15t]){faint} ' + // Thread
-                            '%clr(%-40.40logger{39}){cyan} %clr(:){faint} ' + // Logger
-                            '%m%n%wex' // Message
-                }
-            }
-            break
-
-
+    appender(a.name, ConsoleAppender) {
+        encoder(PatternLayoutEncoder) {
+            pattern = '%clr(%d{yyyy-MM-dd HH:mm:ss.SSS}){faint} ' + // Date
+                    '%clr(%5p) ' + // Log level
+                    '%clr(---){faint} %clr([%15.15t]){faint} ' + // Thread
+                    '%clr(%-40.40logger{39}){cyan} %clr(:){faint} ' + // Logger
+                    '%m%n%wex' // Message
+        }
     }
 }
+
 
 root(WARN, [TOMCAT_LOG])
 logger('au.org.ala.volunteer.BVPServletFilter', INFO, [ACCESS], false)
